198a3cfb214e47b2a2ea72e9051409f65afa5994
Luca <lucaccorey@gmail.com>
2019-10-15 09:38:01 -0700

    If statement to check if input has been made

--- webapp.py
+++ webapp.py
@@ -1,24 +1,33 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_fuel_economy():
-    mpg = request.args['fuel_economy']
-    kmpl = mpg * .425144
-    return render_template('mpg_kmpl.html', responseFromServer = kmpl)
+    if('fuel_economy' in request.args):
+        mpg = request.args['fuel_economy']
+        kmpl = mpg * .425144
+        return render_template('mpg_kmpl.html', responseFromServer = kmpl)
+    else:
+        return render_template('mpg_kmpl.html')
 
 @app.route("/pressure") #annotations tell which function goes with which request
 def render_pressure():
-    atmosphere = request.args['pressure']
-    torr = atmosphere * 760
-    return render_template('atm_torr.html', responseFromServer = torr)
+    if('pressure' in request.args):
+        atmosphere = request.args['pressure']
+        torr = atmosphere * 760
+        return render_template('atm_torr.html', responseFromServer = torr)
+    else:
+        return render_template('atm_torr.html')
 
 @app.route("/volume")
 def render_volume():
-    gallon = request.args['volume']
-    liter = gallon * 3.78541
-    return render_template('gal_lit.html', responseFromServer = liter)
+    if('volume' in request.args):
+        gallon = request.args['volume']
+        liter = gallon * 3.78541
+        return render_template('gal_lit.html', responseFromServer = liter)
+    else:
+        return render_template('gal_lit.html')
 
 if __name__=="__main__":
     app.run(debug=False)
